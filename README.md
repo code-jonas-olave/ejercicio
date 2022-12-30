2c0908e5ff5b41a40dc42099204a1d9f36f3aa47
code1 <code.jonas.olave@gmail.com>
2022-12-30 16:04:16 -0300

    main

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-+ npm init -y
+node.js
+
+Versión v8.10.0
+
 + npm install md5 --save
 
 + Ejecutar proyecto: node index.js
--- index.js
+++ index.js
@@ -4,41 +4,42 @@ const https = require('https');
 const params = require('./files/params.js'); // Parámetros.
 const digitalSignature = require('./files/digitalSignature.js'); // Generación de firma digital.
 
-// Contenido del reporte a enviar:
-var values = {
-    "loginCode":"98173", // Constante.
-    "reportDate":new Date(),
-    "reportType":"2",
-    "latitude":Math.floor(Math.random() * -100) + -1,
-    "longitude":Math.floor(Math.random() * -100) + -1,
-    "gpsDop":1.0,
-    "gpsSatellites":3,
-    "heading":335,
-    "speed":Math.floor(Math.random() * 100) + -1,
-    "speedLabel":"GPS",
-    "text":"Jonas Olave", // Constante.
-    "textLabel":"TAG" // Constante.
-};
-var jsonValues = JSON.stringify(values); // Conversión a JSON string.
+retryPattern(); // <--- Se realiza el envío del primer paquete.
 
-// Datos de conexión.
-const requestOptions = {
-    hostname: params.hostname,
-    port: 443,
-    path: '/frame',
-    method: 'PUT',
-    headers: {
-        'content-Type': 'application/json; charset=UTF-8',
-        'authorization': 'SWSAuth application="'+params.application+'", signature="'+digitalSignature.hashBase64+'", timestamp="'+digitalSignature.timestamp+'"',
-        'Content-Length': Buffer.byteLength(jsonValues)
+setInterval(() => retryPattern(), 60000); // <--- Posteriormente, los paquetes siguientes se envían cada 60 segundos.
+
+function retryPattern(time = 10000) {
+
+    // Contenido del reporte a enviar:
+    // Para este ejemplo, los datos del paquete se generan aleatoriamente dentro de la misma función de envío.
+    var values = {
+        "loginCode":"98173", // Constante.
+        "reportDate":new Date(),
+        "reportType":"2",
+        "latitude":Math.floor(Math.random() * -100) + -1,
+        "longitude":Math.floor(Math.random() * -100) + -1,
+        "gpsDop":1.0,
+        "gpsSatellites":3,
+        "heading":335,
+        "speed":Math.floor(Math.random() * 100) + -1,
+        "speedLabel":"GPS",
+        "text":"Jonas Olave", // Constante.
+        "textLabel":"TAG" // Constante.
+    };
+    var jsonValues = JSON.stringify(values); // Conversión a JSON string.
+
+    // Datos de conexión.
+    var requestOptions = {
+        hostname: params.hostname,
+        port: 443,
+        path: '/frame',
+        method: 'PUT',
+        headers: {
+            'content-Type': 'application/json; charset=UTF-8',
+            'authorization': 'SWSAuth application="'+params.application+'", signature="'+digitalSignature.hashBase64+'", timestamp="'+digitalSignature.timestamp+'"',
+            'Content-Length': Buffer.byteLength(jsonValues)
+        }
     }
-}
-
-setTimeout(() => {
-    retryPattern(requestOptions);    
-}, 11000);
-
-function retryPattern(requestOptions, time = 10000) {
 
     return new Promise((resolve, reject) => {
         const request = https.request(requestOptions, (response) => {
@@ -46,6 +47,8 @@ function retryPattern(requestOptions, time = 10000) {
             //console.log(`Cabeceras de la respuesta: ${JSON.stringify(response.headers)}`);
             response.setEncoding('utf8');
 
+            // En caso de obtener una respuesta desde el servidor 429 o igual o mayor a 500 se hace uso del patrón retry.
+            // Se reintenta cada 10 segundos el envío del paquete hasta obtener una respuesta diferente a 429 o mayor a 500.
             if (response.statusCode == 429 || response.statusCode >= 500) {
                 setTimeout(() => {
                     return retryPattern(requestOptions);
